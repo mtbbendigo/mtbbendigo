c9fded1cf797564412d3280ca59ab9174a0391d6
holly <garethinbendigo@gmail.com>
2014-12-03 20:52:51 +1100

    Update alignment to top and bottom news articles

--- blocks/text_and_picture_news/templates/Text Bottom Image Top/view.php
+++ blocks/text_and_picture_news/templates/Text Bottom Image Top/view.php
@@ -16,7 +16,7 @@ $nh = Loader::helper('navigation');
         </div>
          <div class="row">
             <div class="large-12 columns">
-                <div class="article-image-container">
+                <div class="article-image-container-horizontal">
                     <?php  if (!empty($field_3_image)): ?>
                         <?php  if (!empty($field_3_image_externalLinkURL)) { ?><a href="<?php  echo $this->controller->valid_url($field_3_image_externalLinkURL); ?>" target="_blank"><?php  } ?><img src="<?php  echo $field_3_image->src; ?>" width="<?php  echo $field_3_image->width; ?>" height="<?php  echo $field_3_image->height; ?>" alt="" style="border: 4px solid white; -moz-box-shadow: 0 0 8px #888; -webkit-box-shadow: 0 0 8px #888;
 							-o-box-shadow: 0 0 8px #888; box-shadow: 0 0 8px #888;"/>
