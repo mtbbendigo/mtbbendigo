697cc1e79fba43033d4bb030e215dc37e4b150a3
holly <garethinbendigo@gmail.com>
2014-12-06 14:59:04 +1100

    Re-code Popup Picture block

--- blocks/pop_up_picture/auto.js
+++
@@ -1,9 +0,0 @@
-ccmValidateBlockForm = function() {
-	
-	if ($('#field_2_image_fID-fm-value').val() == '' || $('#field_2_image_fID-fm-value').val() == 0) {
-		ccm_addError('Missing required image: Image');
-	}
-
-
-	return false;
-}
