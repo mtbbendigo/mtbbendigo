dc189d6802c84aa201f81fe4da484d42fc086748
holly <garethinbendigo@gmail.com>
2014-12-06 11:59:42 +1100

    Change image block code to be more readable

--- blocks/mtb_image_external_url/view.php
+++ blocks/mtb_image_external_url/view.php
@@ -2,7 +2,9 @@
 ?>
 
 <?php  if (!empty($field_1_image)): ?>
-	<?php  if (!empty($field_1_image_externalLinkURL)) { ?><a href="<?php  echo $this->controller->valid_url($field_1_image_externalLinkURL); ?>" target="_blank"><?php  } ?><img src="<?php  echo $field_1_image->src; ?>" width="<?php  echo $field_1_image->width; ?>" height="<?php  echo $field_1_image->height; ?>" alt="" style="border: 4px solid white; -moz-box-shadow: 0 0 8px #888; -webkit-box-shadow: 0 0 8px #888; -o-box-shadow: 0 0 8px #888; box-shadow: 0 0 8px #888;"/>
+	<?php  if (!empty($field_1_image_externalLinkURL)) { ?>
+        <a href="<?php  echo $this->controller->valid_url($field_1_image_externalLinkURL); ?>" target="_blank"><?php  } ?>
+        <img src="<?php  echo $field_1_image->src; ?>" width="<?php  echo $field_1_image->width; ?>" height="<?php  echo $field_1_image->height; ?>" alt="" style="border: 4px solid white; -moz-box-shadow: 0 0 8px #888; -webkit-box-shadow: 0 0 8px #888; -o-box-shadow: 0 0 8px #888; box-shadow: 0 0 8px #888;"/>
 	<?php  if (!empty($field_1_image_externalLinkURL)) { ?></a><?php  } ?>
 <?php  endif; ?>
 
